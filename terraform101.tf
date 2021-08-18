1318893c058400acaa0921fef9b18cd70d71e4d5
jbadru73 <j.badru@outlook.com>
2021-08-18 23:59:34 +0100

    new instance commkited

--- terraform101.tf
+++ terraform101.tf
@@ -15,15 +15,15 @@ provider "aws" {
 }
 
 data "template_file" "ec2_user_data" {
-  template = "${file("${path.module}/bootstrap.txt")}"
+  template = file("${path.module}/bootstrap.txt")
 }
 resource "aws_instance" "app_server" {
   ami           = "ami-02b4e72b17337d6c1"
   instance_type = "t2.micro"
   key_name      = "GeneralKP"
-  user_data     = "${data.template_file.ec2_user_data.template}"
+  user_data     = data.template_file.ec2_user_data.template
 
   tags = {
-    Name = "WebServer10"
+    Name = "WebServer101"
   }
 }
