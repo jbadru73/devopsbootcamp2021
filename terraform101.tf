cb8aa0cbdd8430135cc7424814339b65761fb3d3
jbadru73 <j.badru@outlook.com>
2021-08-18 21:46:43 +0100

    --- Auto-Git Commit ---

---
+++ terraform101.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
+data "template_file" "ec2_user_data" {
+  template = "${file("${path.module}/bootstrap.txt")}"
+}
+resource "aws_instance" "app_server" {
+  ami           = "ami-02b4e72b17337d6c1"
+  instance_type = "t2.micro"
+  key_name      = "GeneralKP"
+  user_data     = "${data.template_file.ec2_user_data.template}"
+
+  tags = {
+    Name = "WebServer2"
+  }
+}
