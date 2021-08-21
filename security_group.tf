9df7ad9f02daeb50b3551f18a17afa780b58da0c
jbadru73 <j.badru@outlook.com>
2021-08-21 20:04:20 +0100

    Newly updated  SG file

--- security_group.tf
+++ security_group.tf
@@ -4,13 +4,13 @@ resource "aws_security_group" "web_server_sg" {
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
