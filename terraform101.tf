97df0e4f00696f40866b7fd8ced975445704c97c
jbadru73 <j.badru@outlook.com>
2021-08-21 23:00:53 +0100

    adjusted commit

--- terraform101.tf
+++ terraform101.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "web_server" {
   ami                    = var.image_id
   instance_type          = var.instance_type
   key_name               = var.ssh_key
-  user_data              = data.template_file.ec2_user_data.template
+  user_data              = "${data.template_file.ec2_user_data.template}"
   vpc_security_group_ids = [aws_security_group.web_server_sg.id]
   tags = {
     Name = var.tag_name
