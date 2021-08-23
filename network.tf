89d10b471939b98d1679f543b8778d4c418e9a8b
jbadru73 <j.badru@outlook.com>
2021-08-23 13:04:27 +0100

    --- Auto-Git Commit ---

--- network.tf
+++ network.tf
@@ -1,3 +1,44 @@
 resource "aws_vpc" "demo_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "PersonalVPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "Pubsubnet1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Pubsubnet2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Prisubnet1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "Prisubnet2"
+  }
 }
