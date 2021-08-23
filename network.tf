a9729ca2455b6e747a8d1006e8dc3a7f7a0decb2
jbadru73 <j.badru@outlook.com>
2021-08-23 14:05:28 +0100

    --- Auto-Git Commit ---

--- network.tf
+++ network.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "public_subnet_1" {
   vpc_id                          = aws_vpc.demo_vpc.id
   cidr_block                      = "10.0.0.0/24"
   availability_zone               = "eu-west-1a"
-  map_customer_owned_ip_on_launch = "true"
+  map_public_ip_on_launch = "true"
   tags = {
     Name = "Pubsubnet1"
   }
@@ -21,7 +21,7 @@ resource "aws_subnet" "public_subnet_2" {
   vpc_id                          = aws_vpc.demo_vpc.id
   cidr_block                      = "10.0.1.0/24"
   availability_zone               = "eu-west-1b"
-  map_customer_owned_ip_on_launch = "true"
+  map_public_ip_on_launch = "true"
   tags = {
     Name = "Pubsubnet2"
   }
